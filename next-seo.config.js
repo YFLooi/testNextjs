ca709761f9cdb2136d9186b8616a8e0216823c47
YF Looi <yihfoo@gmail.com>
2022-08-27 22:24:59 +0800

    Fix erros

---
+++ next-seo.config.js
@@ -0,0 +1,12 @@
+export default {
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    site_name: "my-test-side"
+  },
+  twitter: {
+    handle: "@my-test-side.com",
+    site: "@my-test-side.com",
+    cardType: "summary_large_image"
+  }
+};
